a08b726ae1005d6b651065bf975ee587a93475f2
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-08-26 18:54:29 +0900

    home 기초 css 정리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 //함수형 컴포넌트
 import React from "react"
+import './css/Home.css';
 
-function Home(){
-    return <h1>Home 화면 입니다 ㅁㄴㅇㅇㅁ</h1>;
+const Home = () => {
+
+
+    return (
+        <div class="item-list">
+            <div class="item">1</div>
+            <div class="item">2</div>
+            <div class="item">3</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+            <div class="item">4</div>
+        </div>
+    )
 }
 
 export default Home; 
