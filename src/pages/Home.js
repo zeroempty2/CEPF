2d54204b98f8ffcd685cd574d6f84ee026f932d0
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-08-30 20:36:58 +0900

    update homepage css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,29 @@ import axios from 'axios';
 
 import { URL_VARIABLE } from "./export/ExportUrl"
 
-const Product = ({ product }) => {  
-    return(
-        <li className="item">
-            <img src={product.productImg} alt="" />
-        </li>
-    )
-}
+const Product = ({ product }) => {
+    const formatPrice = (price) => {
+        if (price.includes('원')) {
+            return price.replace('원', '');
+        }
+        return price;
+    };
 
+    const formattedPrice = formatPrice(product.productPrice);
+
+    return (
+        <li className="item">
+            <div className='product-img'> 
+                <img src={product.productImg} alt="" />
+            </div>
+            <div className='product-name'>{product.productName}</div>
+            <div className='product-price'>
+                <span className="bold">{formattedPrice}</span><span>원</span>
+            </div>
+        </li>
+    );
+};
+   
 const Home = () => {
     const [products, setProducts] = useState([]);
 
@@ -22,6 +37,7 @@ const Home = () => {
     const fetchProducts = async () => {
         try {
             const response = await axios.get(`${URL_VARIABLE}products?page=${0}&size=10`);
+            console.log(response);
             const data = Array.isArray(response.data.content) ? response.data.content : [];
             setProducts(data);
         } catch (error) {
