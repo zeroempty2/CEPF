7c94a7474b4c0ea72922d6e60c0da809dcd9be79
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-09-02 20:36:18 +0900

    메인페이지 css 수정

--- src/App.js
+++ src/App.js
@@ -7,29 +7,84 @@ import './App.css';
 //검색 api추가
 //로그인, 회원관련 api추가
 function App() {
+  const homeIcon = '/home.png'
+  const searchIcon = '/search.png'
+  const favoriteIcon = '/star.png'
+  const userIcon = '/user.png'
+  const allIcon = '/All.png'
+  const cuIcon = '/CU.png'
+  const emart24Icon = '/Emart24.png'
+  const gs25Icon = '/GS25.png'  
+  const searchIcon2 = '/search2.png'
+
   return (
     <div className="App">
       <div className = "search-bar">
-        <p>CEP</p>
+        <div className="search-bar-logo">
+          <span>편</span>
+        </div>
+        
                 <input
-                  className='comment-text-input'
+                  className='search-bar-text-input'
                   type="text"
                   placeholder="검색어를 입력하세요"
                 />
-                <button type="submit" className='comment-submit-button'>검색</button>
+                <div className="submit-search">
+                  <img src={searchIcon2} alt="submit-search" className="submit-search-img" />
+                </div>
       </div>
       <div className = "wrapper">
       <div className = "store-type">
-      <p>전체아이콘|</p><p>cu아이콘|</p><p>gs아이콘|</p><p>emart아이콘</p>
+        <div className="store-type-icon">
+            <img src={allIcon} alt="all" className="store-type-img" />
+            <span>전체</span>
+        </div>
+        <div className="store-type-icon">
+            <img src={cuIcon} alt="cu" className="store-type-img" />
+            <span>CU</span>
+        </div>
+        <div className="store-type-icon">
+            <img src={gs25Icon} alt="gs25" className="store-type-img" />
+            <span>GS25</span>
+        </div>
+        <div className="store-type-icon">
+            <img src={emart24Icon} alt="emart24" className="store-type-img" />
+            <span>EMART24</span>
+        </div>
       </div>
 
       <div className = "event-type">
-      <p>전체 |</p><p>1+1 |</p><p>2+1 |</p><p>기타(storetype에 따라 변동)</p>
+        <div className = "event-type-tab">
+          <span>전체</span>
         </div>
+        <div className = "event-type-tab">
+          <span>1+1</span>
+        </div>
+        <div className = "event-type-tab">
+          <span>2+1</span>
+        </div>
+        <div className = "event-type-tab">
+          <span>세일</span>
+        </div>
+        <div className = "event-type-tab">
+          <span>덤증정</span>
+        </div>
+      </div>
     </div>
 
     <div className="under-bar">
-      <p>홈|</p><p>즐겨찾기|</p><p>검색|</p><p>회원</p>
+      <div className="under-bar-icon">
+        <img src={homeIcon} alt="home" className="under-bar-img" />
+      </div>
+      <div className="under-bar-icon">
+      <img src={favoriteIcon} alt="favorite" className="under-bar-img" />
+      </div>
+      <div className="under-bar-icon">
+      <img src={searchIcon} alt="search" className="under-bar-img" />
+      </div>
+      <div className="under-bar-icon">
+      <img src={userIcon} alt="user" className="under-bar-img" />
+      </div>
   </div>
     <Routes>
       <Route path="/" element={<Home />} />
