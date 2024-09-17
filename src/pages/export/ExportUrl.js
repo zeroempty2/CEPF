ecae75007a3ed558cff0c9d0af88cee15c4d5360
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-09-17 19:21:01 +0900

    서버 ip 세팅

--- src/pages/export/ExportUrl.js
+++ src/pages/export/ExportUrl.js
@@ -1,7 +1,6 @@
 //localTest
-export const URL_VARIABLE = 'http://localhost:8080/'
+// export  URL_VARIABLE = 'http://localhost:8080/'
 
 
-// //server upload
-// let URL_VARIABLE = 'https://'
-// export default URL_VARIABLE
+//server upload
+export const URL_VARIABLE = 'http://52.79.209.72:8080/'
